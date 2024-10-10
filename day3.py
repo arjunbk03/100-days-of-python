cbfd9ea200c074d9e799e4a37345bac146715ea8
Arjun B K <104476749+arjunbk03@users.noreply.github.com>
2024-10-09 20:47:57 -0400

    day3

---
+++ day3.py
@@ -0,0 +1,12 @@
+#My goal for today is to build a treasure island where users decide the outcome of the games but in order to do that
+# we will start with foundational knowledge on conditional statements:
+
+#=======================================================================================
+height = int(input("What is your height in cm? "))
+if height > 120:
+    print("can ride")
+else:
+    print("cannot ride")
+
+
+
